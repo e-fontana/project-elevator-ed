a9dca68df3a5645db3d5b0c0ce60678520c18ed8
João Kishimoto <joaov.kishimoto@gmail.com>
2024-08-08 20:51:58 -0300

    fix: button to led

--- handlers/door_status/design.v
+++ handlers/door_status/design.v
@@ -1,4 +1,4 @@
-module door_status #(parameter st_floor = 2'b00, nd_floor = 2'b01, rd_floor = 2'b10) (state, st_button, nd_button, rd_button, open_door);
+module door_status #(parameter st_floor = 2'b00, nd_floor = 2'b01, rd_floor = 2'b10) (state, st_led, nd_led, rd_led, open_door);
     
     input [1:0] state;
     inout reg st_button, nd_button, rd_button;
@@ -6,22 +6,22 @@ module door_status #(parameter st_floor = 2'b00, nd_floor = 2'b01, rd_floor = 2'
 
     always @(state or st_button or nd_button or rd_button) begin
 
-        if (state == st_floor & st_button) begin
+        if (state == st_floor & st_led) begin
             open_door <= 1;
             #2 open_door <= 0;
-            st_button <= 0;
+            st_led <= 0;
         end
         
-        elif (state == nd_floor & nd_button) begin
+        elif (state == nd_floor & nd_led) begin
             open_door <= 1;
             #2 open_door <= 0;
-            nd_button <= 0;
+            nd_led <= 0;
         end
         
-        elif (state == rd_floor & rd_button) begin
+        elif (state == rd_floor & rd_led) begin
             open_door <= 1;
             #2 open_door <= 0;
-            rd_button <= 0;
+            rd_led <= 0;
         end
         
         else begin
