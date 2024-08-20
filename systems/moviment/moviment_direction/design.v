7e0c91c0fa471ccfa57192f37c928fcc4b57b471
João Kishimoto <joaov.kishimoto@gmail.com>
2024-08-20 01:26:57 -0300

    add: moviment_direction

--- systems/moviment/moviment_direction/design.v
+++ systems/moviment/moviment_direction/design.v
@@ -1,3 +1,12 @@
-module moviment_direction;
-    
+module moviment_direction (floor, next_floor, direction);
+    input [1:0] floor, next_floor;
+    output reg direction;
+    parameter going_up = 1'b1, going_down = 1'b0;
+
+    if (floor < next_floor) begin
+        direction = going_up;
+    end else if (floor > next_floor) begin
+        direction = going_down;
+    end
+    // TODO: If the floor is the same as the next floor, the elevator should stop, but not in this module.
 endmodule
