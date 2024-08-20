a7055f6cb195ca4fc354b43f7f0f482d1029565b
e-fontana <contato@eduardofontana.dev>
2024-08-20 01:37:19 -0300

    feat: door block design and tb

--- systems/emergency/doors_block/design.v
+++ systems/emergency/doors_block/design.v
@@ -1,3 +1,8 @@
-module door_block;
+module door_block(sos_mode, door_block);
+    input sos_mode;
+    output reg door_block = 1;
+
+    always @(sos_mode) door_block = sos_mode;
     
+
 endmodule
--- systems/emergency/doors_block/testbench.v
+++ systems/emergency/doors_block/testbench.v
@@ -1,3 +1,17 @@
+`include "systems/emergency/doors_block/design.v"
+
 module door_block_tb;
-    
+    reg sos_mode;
+    wire door_block;
+
+    door_block DB(.sos_mode(sos_mode), .door_block(door_block));
+
+    always #1 sos_mode = ~sos_mode;
+
+    initial begin
+        sos_mode = 0;
+        $monitor("door_block = %b, sos_mode = %b", door_block, sos_mode);
+        #10 $finish;
+    end
+
 endmodule
