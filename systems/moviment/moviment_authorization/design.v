de7cd35f6590c0830dde5768be85045033608e97
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2024-08-22 13:42:43 -0300

    feat: moviment authorization module

--- systems/moviment/moviment_authorization/design.v
+++ systems/moviment/moviment_authorization/design.v
@@ -1,3 +1,9 @@
-module moviment_authorization;
-    
+module moviment_authorization(clk, emergency_mode, moviment_authorization);
+    input clk, emergency_mode;
+    output reg moviment_authorization;
+
+    always @(posedge clk) begin
+        if (emergency_mode) moviment_authorization <= 0;
+        else moviment_authorization <= 1;
+    end
 endmodule
--- systems/moviment/moviment_authorization/testbench.v
+++ systems/moviment/moviment_authorization/testbench.v
@@ -1,3 +1,13 @@
+`include "systems/moviment/moviment_authorization/design.v"
+
 module moviment_authorization_tb;
-    
+    reg clk, emergency_mode;
+    wire moviment_authorization;
+
+    moviment_authorization mv_a(clk, emergency_mode, moviment_authorization);
+
+    initial begin
+        // Quando o modo de emergencia virar 1, a autorização de movimento deve ser 0 instananeamente.
+        // Assim, parando o sistema.
+    end
 endmodule
