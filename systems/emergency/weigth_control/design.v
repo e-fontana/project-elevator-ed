dab05de6cf1f6678b1498b0bd8ebadf09bec14a6
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2024-08-21 23:02:29 -0300

    feat: base emergency testbench + folder in build created

--- systems/emergency/weigth_control/design.v
+++ systems/emergency/weigth_control/design.v
@@ -8,15 +8,15 @@ module weight_control
     input clk, weight_flip, reset_weight_flip;
     output reg weight_limit_exceeded;
 
-    reg [2:0] counter = 3`b000;
+    reg [2:0] counter = 3'b000;
 
     always @(posedge reset_weight_flip) begin
-        counter <= 3`b000;
+        counter <= 3'b000;
         weight_limit_exceeded <= 1'b0;
     end
 
     always @(posedge weight_flip) begin
-        if (counter <= 3`b101) counter <= counter + 1;
+        if (counter <= 3'b101) counter <= counter + 1;
         else weight_limit_exceeded <= 1'b1;
     end
 endmodule
