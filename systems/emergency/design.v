4f23ba7493908c9e8c79a79bd11edae6f259b0c6
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2024-08-22 01:12:54 -0300

    char: replaced doors_block folder location

--- systems/emergency/design.v
+++ systems/emergency/design.v
@@ -1,13 +1,19 @@
 `include "./systems/emergency/sos_handler/design.v"
 `include "./systems/emergency/weigth_control/design.v"
+`include "./systems/emergency/doors_block/design.v"
 
 module emergency_module (
     clk,
-    sos_flip,
+    emergency_flip,
+    emergency_mode,
     weight_flip,
     weight_flip_reset,
     weight_limit_exceeded,
+    doors_block,
 );
+    input clk, emergency_flip, weight_flip, weight_flip_reset;
+    output reg weight_limit_exceeded, doors_block, emergency_mode;
+
     weight_control wc(
         .weight_flip(weight_flip),
         .weight_flip_reset(weight_flip_reset),
@@ -15,7 +21,10 @@ module emergency_module (
     );
     sos_handler sh(
         .clk(clk),
-        .sos_flip(sos_flip),
-        .sos_mode(sos_mode)
+        .sos_flip(emergency_flip),
+        .sos_mode(emergency_mode)
+    );
+    doors_block db(
+        .sos_mode(emergency_mode),
     );
 endmodule
