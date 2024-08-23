1f2312fb56a7b269fa5865bcfd4be676459be137
Amanda Bastos <amandabastoscorreia@gmail.com>
2024-08-23 08:35:49 -0300

    test: add testbench weight_control

--- systems/emergency/weigth_control/testbench.v
+++ systems/emergency/weigth_control/testbench.v
@@ -1,18 +1,33 @@
-`include "./systems/emergency/weigth_control/design.v"
-
 module weight_control_tb;
-    wire weight_flip, reset_weight_flip, weight_limit_exceeded;
-
-    weight_control wc(
+reg weight_flip;
+reg weight_flip_reset;
+reg weight_limit_exceed;
+reg [2:0] counter;
+  
+weight_control wc (
         .weight_flip(weight_flip),
-        .reset_weight_flip(reset_weight_flip),
-        .weight_limit_exceeded(weight_limit_exceeded)
+        .weight_flip_reset(weight_flip_reset),
+  		.weight_limit_exceeded(weight_limit_exceeded)
     );
 
-    initial begin
-        // change the value of weight_flip to 1 5 times to test the weight_limit_exceeded
-        // after the 5th time, the weight_limit_exceeded should be 1
-        // boa sorte mandy
-    end
+initial begin
+    weight_flip = 0;
+    weight_flip_reset = 1;
+  
+   #200;
+       
+   $finish;
+  
+end
+
+always #5 weight_flip = ~weight_flip;
+always #50 weight_flip_reset = ~weight_flip_reset;
+
+always @(posedge weight_flip) begin
+  if (weight_limit_exceeded <= 1'b0)    
+    $display("O limite de peso não foi excedido - weight_limit_exceeded: %b", weight_limit_exceeded);
+    else
+    $display("O limite de peso foi excedido - weight_limit_exceeded: %b", weight_limit_exceeded);
+end
 
 endmodule
