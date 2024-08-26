e7eb611b22acd344ed7ea6a4410e7fb0b1ccc1f6
João Kishimoto <joaov.kishimoto@gmail.com>
2024-08-26 00:09:14 -0300

    feat: door_block now blocks the door closed

--- systems/moviment/door_status/design.v
+++ systems/moviment/door_status/design.v
@@ -1,45 +1,60 @@
-module door_status #(parameter st_floor = 2'b00, nd_floor = 2'b01, rd_floor = 2'b10) (floor, next_floor, st_led, nd_led, rd_led, door_status, is_mooving, open_when);
+`include "../../emergency/doors_block/design.v"
+
+module door_status #(parameter st_floor = 2'b00, nd_floor = 2'b01, rd_floor = 2'b10) (floor, next_floor, st_led, nd_led, rd_led, door_status, is_mooving, open_when, is_door_blocked, clk);
     
     input [1:0] floor;
+    input clk;
     output st_led, nd_led, rd_led;
     output reg door_status;
     inout is_mooving;
+    
+    door_block blk (sos_mode);
 
-    // TODO: Call emergency module to verify door status
+    // Done: Call emergency module to verify door status
 
-    always @(next_floor) begin
+    always @(clk | next_floor) begin
 
-        if (open_when == st_floor & floor == st_floor) begin
-            is_mooving = 1'b0;
-            door_status = 2'b01; // opening door
-            door_status = 2'b10; // open door
-            door_status = 2'b11; // closing door
-            door_status = 2'b00; // door closed
-            st_led = 1'b0;
-            is_mooving = 1'b1;
-        end
+        if(!blk) begin
+
+            if (open_when == st_floor & floor == st_floor) begin
+                is_mooving = 1'b0;
+                door_status = 2'b01; // opening door
+                door_status = 2'b10; // open door
+                door_status = 2'b11; // closing door
+                door_status = 2'b00; // door closed
+                st_led = 1'b0;
+                is_mooving = 1'b1;
+            end
         
-        else if (open_when == nd_floor & floor == nd_floor) begin
-            is_mooving = 1'b0;
-            door_status = 2'b01; // opening door
-            door_status = 2'b10; // open door
-            door_status = 2'b11; // closing door
-            door_status = 2'b00; // door closed
-            nd_led = 1'b0;            
-            is_mooving = 1'b1;
-        end
-        
-        else if (open_when == rd_floor & floor == rd_floor) begin
-            is_mooving = 1'b0;
-            door_status = 2'b01; // opening door
-            door_status = 2'b10; // open door
-            door_status = 2'b11; // closing door
-            door_status = 2'b00; // door closed
-            rd_led = '1b0;
-            is_mooving = 1'b1;
-        end
-        
+            else if (open_when == nd_floor & floor == nd_floor) begin
+                is_mooving = 1'b0;
+                door_status = 2'b01; // opening door
+                door_status = 2'b10; // open door
+                door_status = 2'b11; // closing door
+                door_status = 2'b00; // door closed
+                nd_led = 1'b0;            
+                is_mooving = 1'b1;
+            end
+            
+            else if (open_when == rd_floor & floor == rd_floor) begin
+                is_mooving = 1'b0;
+                door_status = 2'b01; // opening door
+                door_status = 2'b10; // open door
+                door_status = 2'b11; // closing door
+                door_status = 2'b00; // door closed
+                rd_led = '1b0;
+                is_mooving = 1'b1;
+            end
+            
+            else begin
+                door_status = 2'b00;
+            end
+        end 
+        else if (2'b00) begin
+            door_status = 2'b00;
+        end 
         else begin
+            door_status = 2'b11;
             door_status = 2'b00;
         end
     end
