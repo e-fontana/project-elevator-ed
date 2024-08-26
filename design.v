82bf5d209e344877c11c03faa14b26b857f3321d
André <atipicode.ec@gmail.com>
2024-08-26 11:56:11 -0300

    working

--- design.v
+++ design.v
@@ -5,6 +5,6 @@ module TOP #(parameter clk_freq = 10) (clk_50, clk, led1, led2, led3, floor1, fl
 	input clk_50, clk, led1, led2, led3;
 	output floor1, floor2, floor3, door, moving;
 
-	frequency #(clk_freq) FREQUENCY(clk_50, led1, led2, led3, clk);
+	frequency #(clk_freq) FREQUENCY(clk_50, led1, led2, led3, moving, clk);
 	movement MOVEMENT(clk, led1, led2, led3, floor1, floor2, floor3, door, moving);
 endmodule
--- frequency.v
+++ frequency.v
@@ -1,21 +1,18 @@
-module frequency #(parameter clk_freq = 50000000) (clk_50, led1, led2, led3, clk);
-	input clk_50, led1, led2, led3;
+module frequency #(parameter clk_freq = 50000000) (clk_50, led1, led2, led3, moving, clk);
+	input clk_50, led1, led2, led3, moving;
 	output reg clk;
 
-	reg auth = 1'b0;
 	reg [25:0] counter;
 
 	always @(posedge led1 or posedge led2 or posedge led3) begin
-		auth <= 1'b1;
 		counter <= 0;
 	end
 	
 	always @(posedge clk_50) begin
-		if (counter < clk_freq && auth) begin
+		if (counter < clk_freq && moving) begin
 			counter <= counter + 1;
 			clk <= 0;
 		end else begin
-			auth = 1'b0;
 			counter <= 0;
 			clk <= ~clk;
 		end
