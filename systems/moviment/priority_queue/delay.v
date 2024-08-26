c41cbac381901a0a9fa541eddb1bfdce5cde8675
João Kishimoto <joaov.kishimoto@gmail.com>
2024-08-26 00:30:19 -0300

    fix: inout reg -> inout

--- systems/moviment/priority_queue/delay.v
+++ systems/moviment/priority_queue/delay.v
@@ -1,16 +1,16 @@
 module delay (clk, delay, count, pass);
     input clk;
     input [2:0] delay;
-    inout reg pass;
-    inout reg [2:0] count;
+    output reg pass;
+    inout [2:0] count;
     
     always @(posedge clk) begin
         if (count == delay) begin
-            pass = 1'b1;
-            count = 3'b000;
+            pass <= 1'b1;
+            count <= 3'b000;
         end else begin
-            pass = 1'b0;
-            count = count + 1;
+            pass <= 1'b0;
+            count <= count + 1;
         end
     end
 endmodule
